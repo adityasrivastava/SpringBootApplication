a2ffc18c5965de3781157d53fbdb7d8d68afb6f4
adityasrivastava <adityasrivastava010@gmail.com>
2016-02-08 11:47:49 +0530

    Transactional Added

--- src/main/java/com/springbootapplication/service/UserDetailServiceBean.java
+++ src/main/java/com/springbootapplication/service/UserDetailServiceBean.java
@@ -4,11 +4,15 @@ import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.springbootapplication.model.User;
 import com.springbootapplication.ws.repository.UserRepository;
 
 @Service
+@Transactional(
+		propagation = Propagation.SUPPORTS, readOnly=true)
 public class UserDetailServiceBean implements UserDetailService {
 	
 	@Autowired
@@ -20,11 +24,15 @@ public class UserDetailServiceBean implements UserDetailService {
 	}
 
 	@Override
+	@Transactional(
+			propagation = Propagation.REQUIRED, readOnly=false)
 	public User create(User user) {
 		return userRepository.save(user);
 	}
 
 	@Override
+	@Transactional(
+			propagation = Propagation.REQUIRED, readOnly=false)
 	public User update(User user) {
 		
 		if(user.getId() == null){
@@ -35,6 +43,8 @@ public class UserDetailServiceBean implements UserDetailService {
 	}
 
 	@Override
+	@Transactional(
+			propagation = Propagation.REQUIRED, readOnly=false)
 	public void delete(Long id) {
 		userRepository.delete(id);
 	}
